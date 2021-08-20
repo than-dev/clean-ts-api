ee3318e621e319ed460d9f43e2061635c81c0150
NathanCotrim <nathan.cotrim@gmail.com>
2021-08-20 06:54:00 -0300

    test: ensure SignUpController returns 500 if emailValidator throws

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -2,6 +2,7 @@ import { SignUpController } from './signup';
 import { MissingParamError } from '../errors/missing-param-error';
 import { InvalidParamError } from '../errors/invalid-param-error';
 import { EmailValidator } from '../protocols/email-validator';
+import { ServerError } from '../errors/server-error';
 
 interface SutTypes {
     sut: SignUpController;
@@ -11,7 +12,7 @@ interface SutTypes {
 const makeSut = (): SutTypes => {
     class EmailValidatorStub implements EmailValidator {
         isValid(email: string): boolean {
-            return true;
+            return false;
         }
     }
 
@@ -106,5 +107,46 @@ describe('SignUp Controller', () => {
 
         expect(httpResponse.statusCode).toBe(400);
         expect(httpResponse.body).toEqual(new InvalidParamError('email'));
+    });
+
+    it('should call emailValidator with correct email', () => {
+        const { sut, emailValidatorStub } = makeSut();
+        const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid');
+
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                password: 'any_password',
+                passwordConfirmation: 'any_password',
+                email: 'any_email@mail.com'
+            }
+        };
+
+        sut.handle(httpRequest);
+        expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
+    });
+
+    it('should return 500 if emailValidator throws', () => {
+        class EmailValidatorStub implements EmailValidator {
+            isValid(email: string): boolean {
+                throw new Error();
+            }
+        }
+
+        const emailValidatorStub = new EmailValidatorStub();
+        const sut = new SignUpController(emailValidatorStub);
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                password: 'any_password',
+                passwordConfirmation: 'any_password',
+                email: 'any_email@mail.com'
+            }
+        };
+
+        const httpResponse = sut.handle(httpRequest);
+
+        expect(httpResponse.statusCode).toBe(500);
+        expect(httpResponse.body).toEqual(new ServerError());
     });
 });
