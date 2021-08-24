fc416d11bb965bae0674674c2274d523580e9e78
NathanCotrim <nathan.cotrim@gmail.com>
2021-08-23 22:34:55 -0300

    test: ensure DbAddAccount throws if AddAccountRepository throws an exception

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -100,4 +100,21 @@ describe('DbAddAccount Usecase', () => {
             password: 'hashed_password'
         });
     });
+
+    it('should throw if AddAccountRepository throws an exception', async () => {
+        const { sut, addAccountRepositoryStub } = makeSut();
+
+        jest.spyOn(addAccountRepositoryStub, 'add').mockReturnValueOnce(
+            new Promise((resolve, reject) => reject(new Error()))
+        );
+
+        const accountData = {
+            name: 'valid_name',
+            email: 'valid_email@mail.com',
+            password: 'valid_password'
+        };
+
+        const promise = sut.add(accountData);
+        await expect(promise).rejects.toThrow();
+    });
 });
