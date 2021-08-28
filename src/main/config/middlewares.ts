4d948e65b4bdf62175eec3b80a968ec1f8521299
NathanCotrim <nathan.cotrim@gmail.com>
2021-08-28 16:33:59 -0300

    feat: ensure content-type default is json or xml when forced

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,10 @@
 import { Express } from 'express';
 import { bodyParser } from '../middlewares/body-parser';
+import { cors } from '../middlewares/cors';
+import { contentType } from '../middlewares/content-type';
 
 export function setupMiddlewares(app: Express): void {
+    app.use(contentType);
     app.use(bodyParser);
+    app.use(cors);
 }
