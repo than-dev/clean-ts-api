75db5a32fa0233c9d8c743bb8089f5c2c4cb1152
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-01 19:14:13 -0300

    refactor: move required field validation to required field composite

---
+++ src/main/factories/signup-validation.spec.ts
@@ -0,0 +1,22 @@
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation';
+import { Validation } from '../../presentation/helpers/validators/validation';
+import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite';
+import { makeSignUpValidation } from './signup-validation';
+
+describe('SignUpValidation Factory', () => {
+    jest.mock('./../presentation/helpers/validators/validation-composite');
+
+    it('should call ValidationComposite with all validations', () => {
+        makeSignUpValidation();
+        const validations: Validation[] = [];
+        for (const field of [
+            'name',
+            'email',
+            'password',
+            'passwordConfirmation'
+        ]) {
+            validations.push(new RequiredFieldValidation(field));
+        }
+        expect(ValidationComposite).toHaveBeenCalledWith(validations);
+    });
+});
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -6,6 +6,7 @@ import { SignUpController } from '../../presentation/controllers/signup/signup';
 import { Controller } from '../../presentation/protocols';
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter';
 import { LogControllerDecorator } from '../decorators/log';
+import { makeSignUpValidation } from './signup-validation';
 
 export const makeSignUpController = (): Controller => {
     const salt = 12;
@@ -18,9 +19,9 @@ export const makeSignUpController = (): Controller => {
     );
     const signUpController = new SignUpController(
         emailValidatorAdapter,
-        dbAddAccount
+        dbAddAccount,
+        makeSignUpValidation()
     );
     const logMongoRepository = new LogMongoRepository();
-
     return new LogControllerDecorator(signUpController, logMongoRepository);
 };
