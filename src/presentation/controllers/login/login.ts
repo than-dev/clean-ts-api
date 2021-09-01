ba91b26d66191e29e115d830c0c91f410da63229
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-01 17:02:16 -0300

    feat: implement correct return if a param is not provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,7 +1,17 @@
+import { MissingParamError } from '../../errors';
+import { badRequest } from '../../helpers/http-helper';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
 
 export class LoginController implements Controller {
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        if (!httpRequest.body.email) {
+            return badRequest(new MissingParamError('email'));
+        }
+
+        if (!httpRequest.body.password) {
+            return badRequest(new MissingParamError('password'));
+        }
+
         return new Promise((resolve) =>
             resolve({
                 statusCode: 200,
