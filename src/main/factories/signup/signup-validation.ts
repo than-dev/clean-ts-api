52081560ba745d62c3b5bb5a465b4b62120af7ff
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-01 23:29:11 -0300

    fix: change import path

--- src/main/factories/signup/signup-validation.ts
+++ src/main/factories/signup/signup-validation.ts
@@ -14,5 +14,6 @@ export const makeSignUpValidation = (): ValidationComposite => {
         new CompareFieldsValidation('password', 'passwordConfirmation')
     );
     validations.push(new EmailValidation('email', new EmailValidatorAdapter()));
+
     return new ValidationComposite(validations);
 };
