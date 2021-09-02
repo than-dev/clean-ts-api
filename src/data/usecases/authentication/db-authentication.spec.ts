d8b9062851560f875fc40d65e72616c8e436d1e2
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-02 15:05:25 -0300

    test: ensure DbAuthentication calss LoadAccountByEmailRepo with correct email

---
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/brace-style */
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository';
+import { AccountModel } from './../../../domain/models/account';
+import { DbAuthentication } from './db-authentication';
+describe('DbAuthentication', () => {
+    interface SutTypes {
+        sut: DbAuthentication;
+        loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository;
+    }
+
+    const makeLoadAccountByEmailRepositoryStub =
+        (): LoadAccountByEmailRepository => {
+            class LoadAccountByEmailRepositoryStub
+                implements LoadAccountByEmailRepository
+            {
+                async load(email: string): Promise<AccountModel> {
+                    const account: AccountModel = {
+                        id: 'any_id',
+                        name: 'any_name',
+                        email: 'any_email@mail.com',
+                        password: 'any_password'
+                    };
+
+                    return new Promise((resolve) => resolve(account));
+                }
+            }
+
+            return new LoadAccountByEmailRepositoryStub();
+        };
+
+    const makeSut = (): SutTypes => {
+        const loadAccountByEmailRepositoryStub =
+            makeLoadAccountByEmailRepositoryStub();
+        const sut = new DbAuthentication(loadAccountByEmailRepositoryStub);
+
+        return {
+            sut,
+            loadAccountByEmailRepositoryStub
+        };
+    };
+
+    it('should call LoadAccountByEmailRepository with correct email', async () => {
+        const { sut, loadAccountByEmailRepositoryStub } = makeSut();
+        const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load');
+        await sut.auth({
+            email: 'any_email@mail.com',
+            password: 'any_password'
+        });
+        expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com');
+    });
+});
