813e273d3d5e7569cbbb27e21a76b0090d7578e9
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-02 15:09:07 -0300

    refactor: create makeFakeAccount

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -8,20 +8,20 @@ describe('DbAuthentication', () => {
         loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository;
     }
 
+    const makeFakeAccount = (): AccountModel => ({
+        id: 'any_id',
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+    });
+
     const makeLoadAccountByEmailRepositoryStub =
         (): LoadAccountByEmailRepository => {
             class LoadAccountByEmailRepositoryStub
                 implements LoadAccountByEmailRepository
             {
                 async load(email: string): Promise<AccountModel> {
-                    const account: AccountModel = {
-                        id: 'any_id',
-                        name: 'any_name',
-                        email: 'any_email@mail.com',
-                        password: 'any_password'
-                    };
-
-                    return new Promise((resolve) => resolve(account));
+                    return new Promise((resolve) => resolve(makeFakeAccount()));
                 }
             }
 
