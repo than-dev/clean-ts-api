f0f510d8a25177b178b75172ada2fcd36ce49245
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-02 16:23:07 -0300

    refactor: move DbAuthentication protocols to a single file

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,11 +1,14 @@
-import { AuthenticationModel } from './../../../domain/usecases/authentication';
 /* eslint-disable @typescript-eslint/brace-style */
-import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
-import { AccountModel } from './../../../domain/models/account';
 import { DbAuthentication } from './db-authentication';
-import { HashComparer } from '../../protocols/criptography/hash-comparer';
-import { TokenGenerator } from '../../protocols/criptography/token-generator';
-import { UpdateAccessTokenRepository } from '../../protocols/db/update-access-token-repository';
+import {
+    AccountModel,
+    LoadAccountByEmailRepository,
+    AuthenticationModel,
+    HashComparer,
+    TokenGenerator,
+    UpdateAccessTokenRepository
+} from './db-authentication-protocols';
+
 describe('DbAuthentication', () => {
     interface SutTypes {
         sut: DbAuthentication;
