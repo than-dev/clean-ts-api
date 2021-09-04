71999cf32e0aff7b7bc713c2af18d08b2629aa9d
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-04 12:43:22 -0300

    feat: ensire JwtAdapter calls sign with correct values

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,15 @@
+import { Secret, sign } from 'jsonwebtoken';
+import { Encrypter } from '../../../data/protocols/criptography/encrypter';
+
+export class JwtAdapter implements Encrypter {
+    private readonly secret: Secret;
+
+    constructor(secret: Secret) {
+        this.secret = secret;
+    }
+
+    async encrypt(value: string): Promise<string> {
+        sign({ id: value }, this.secret);
+        return 'token';
+    }
+}
