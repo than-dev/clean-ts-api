89a1ba89d04cb85d12db3ab44de75817ed9d4517
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-12 14:12:02 -0300

    refactor: change scripts and fix docker-compose

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,12 +1,13 @@
 /* eslint-disable node/no-path-concat */
 import { Express, Router } from 'express';
 import { readdirSync } from 'fs';
+import { join } from 'path';
 
 export function setupRoutes(app: Express): void {
     const router = Router();
     app.use('/api', router);
-    readdirSync(`${__dirname}/../routes`).map(async (file) => {
-        if (!file.includes('.test.')) {
+    readdirSync(join(__dirname, '../routes')).map(async (file) => {
+        if (!file.includes('.test.') && !file.endsWith('.map')) {
             (await import(`../routes/${file}`)).default(router);
         }
     });
