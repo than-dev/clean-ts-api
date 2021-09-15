394e4f56b389a2de3aa15ac6447627ed1e9f3cd2
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-15 19:41:37 -0300

    refactor: change import name bson for mongodb

--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -5,7 +5,7 @@ import { AccountModel } from '../../../../domain/models/account';
 import { MongoHelper } from '../helpers/mongo-helper';
 import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/account/load-account-by-email-repository';
 import { UpdateAccessTokenRepository } from '../../../../data/protocols/db/account/update-access-token-repository';
-import { ObjectId } from 'bson';
+import { ObjectId } from 'mongodb';
 
 export class AccountMongoRepository
     implements
