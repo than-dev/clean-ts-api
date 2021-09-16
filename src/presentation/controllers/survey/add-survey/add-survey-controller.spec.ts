4621917f7c30a6b670d19988eff32c4d6da3cd7e
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-15 21:42:30 -0300

    test: ensure AddSurveyController return 204 on success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -5,7 +5,11 @@ import {
     Validation
 } from './add-survey-controller-protocols';
 import { AddSurveyController } from './add-survey-controller';
-import { badRequest, serverError } from '../../../helpers/http/http-helper';
+import {
+    badRequest,
+    noContent,
+    serverError
+} from '../../../helpers/http/http-helper';
 
 describe('Add Survey Controller', () => {
     interface SutTypes {
@@ -97,5 +101,13 @@ describe('Add Survey Controller', () => {
         const httpResponse = await sut.handle(makeFakeRequest());
 
         expect(httpResponse).toEqual(serverError(new Error()));
+    });
+
+    it('should return 204 if on success', async () => {
+        const { sut } = makeSut();
+
+        const httpResponse = await sut.handle(makeFakeRequest());
+
+        expect(httpResponse).toEqual(noContent());
     });
 });
