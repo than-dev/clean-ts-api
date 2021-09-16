dd7d9bb0689389b350e6861cb53c678dd3114284
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-16 15:22:51 -0300

    feat: ensure add a survey on success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/brace-style */
+
+import { AddSurveyRepository } from '../../../../data/protocols/db/survey/add-survey-repository';
+import { AddSurveyModel } from '../../../../domain/usecases/add-survey';
+import { MongoHelper } from '../helpers/mongo-helper';
+
+export class SurveyMongoRepository implements AddSurveyRepository {
+    // constructor() {}
+
+    async add(surveyData: AddSurveyModel): Promise<void> {
+        const surveyCollection = await MongoHelper.getCollection('surveys');
+        await surveyCollection.insertOne(surveyData);
+    }
+}
