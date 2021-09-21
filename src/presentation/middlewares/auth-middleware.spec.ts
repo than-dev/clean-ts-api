26a32a6aa02922788264d037bfef20c21f0b3a80
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-21 15:28:02 -0300

    feat: create Access Denied error

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,3 +1,28 @@
+import { HttpRequest } from '../protocols/http';
+
 describe('Auth Middleware', () => {
-    it('should  ', () => {});
+    interface SutTypes {
+        sut: AuthMiddleware;
+    }
+
+    const makeFakeRequest = (): HttpRequest => ({
+        headers: {}
+    });
+
+    const makeSut = (): SutTypes => {
+        const sut = new AuthMiddleware();
+
+        return {
+            sut
+        };
+    };
+
+    it('should return 403 if no x-access-token exists in headers', () => {
+        const { sut } = makeSut();
+        const httpRequest: HttpRequest = {};
+
+        const httpResponse = await sut.handle();
+
+        expect(httpResponse).toEqual(forbidden());
+    });
 });
