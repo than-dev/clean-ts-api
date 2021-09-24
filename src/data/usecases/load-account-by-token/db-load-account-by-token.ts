0beb006e7fcfbfa857ee02f1f90a517de5106ea9
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-24 18:47:14 -0300

    refactor: change decrypter return name

--- src/data/usecases/load-account-by-token/db-load-account-by-token.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.ts
@@ -10,9 +10,9 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
     ) {}
 
     async load(token: string, role?: string): Promise<AccountModel> {
-        const decryptedToken = await this.decrypter.decrypt(token);
+        const accessToken = await this.decrypter.decrypt(token);
 
-        if (decryptedToken) {
+        if (accessToken) {
             await this.loadAccountByTokenRepository.loadByToken(token, role);
         }
 
