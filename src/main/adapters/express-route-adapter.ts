c7c83208defc07a81eff64845c7cfd8d54adce60
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-25 15:14:36 -0300

    feat: create express middleware adapter

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,14 +1,17 @@
-import { Controller, HttpRequest } from '../../presentation/protocols';
-import { Request, Response } from 'express';
+import { HttpRequest, Middleware } from '../../presentation/protocols';
+import { NextFunction, Request, Response } from 'express';
 
-export const adaptRoute = (controller: Controller) => {
-    return async (req: Request, res: Response) => {
+export const adaptMiddleware = (middleware: Middleware) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
         const httpRequest: HttpRequest = {
-            body: req.body
+            headers: req.headers
         };
-        const httpResponse = await controller.handle(httpRequest);
-        if (httpResponse.statusCode >= 200 || httpResponse.statusCode <= 299) {
-            res.status(httpResponse.statusCode).json(httpResponse.body);
+
+        const httpResponse = await middleware.handle(httpRequest);
+
+        if (httpResponse.statusCode === 200) {
+            Object.assign(req, httpResponse.body);
+            next();
         } else {
             res.status(httpResponse.statusCode).json({
                 error: httpResponse.body.message
