019a2a05bacdd728db5ab4fd3a395d36a68b4e14
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-25 15:04:49 -0300

    refactor: refact main layer adapter structure

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express';
-import { adaptRoute } from '../adapters/express/express-route-adapter';
+import { adaptRoute } from '../adapters/express-route-adapter';
 import { makeLoginController } from '../factories/controllers/login/login/login-controller-factory';
 import { makeSignUpController } from '../factories/controllers/login/signup/signup-controller-factory';
 
