4ed88c00070ce18d9d3e1eaf61bc79612e854525
NathanCotrim <nathan.cotrim@gmail.com>
2021-09-26 14:48:33 -0300

    refactor: change addSurveyRepository protocol to interface instead class

--- src/data/protocols/db/survey/add-survey-repository.ts
+++ src/data/protocols/db/survey/add-survey-repository.ts
@@ -1,5 +1,5 @@
 import { AddSurveyModel } from '../../../../domain/usecases/add-survey';
 
-export class AddSurveyRepository {
-    async add(surveyData: AddSurveyModel): Promise<void> {}
+export interface AddSurveyRepository {
+    add(surveyData: AddSurveyModel): Promise<void>;
 }
