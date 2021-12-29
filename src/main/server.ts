038798f361213c0bdd375dc4838533a7b6056d65
NathanCotrim <nathan.cotrim@gmail.com>
2021-12-28 21:13:26 -0300

    refactor: up docker-compose correctly

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,14 @@
+/* eslint-disable import/first */
+import 'dotenv/config';
+import { addAlias } from 'module-alias';
+import { join } from 'path';
+
+if (process.env.NODE_ENV === 'production') {
+    addAlias('@', join(__dirname, '..'));
+}
+
 import { MongoHelper } from '@/infra/db/mongodb/helpers/mongo-helper';
 import env from './config/env';
-import 'module-alias/register';
 
 MongoHelper.connect(env.mongoUrl)
     .then(async () => {
