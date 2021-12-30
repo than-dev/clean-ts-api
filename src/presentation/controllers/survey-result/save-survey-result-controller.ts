75bd565423d87ec9ba61739da6b05e3fd8b167ed
NathanCotrim <nathan.cotrim@gmail.com>
2021-12-30 11:51:11 -0300

    feat: ensure SaveSurveyResultController return 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result-controller.ts
@@ -4,18 +4,23 @@ import {
     HttpResponse,
     forbidden,
     InvalidParamError,
-    LoadSurveyById
+    LoadSurveyById,
+    serverError
 } from './save-survey-result-controller-protocols';
 
 export class SaveSurveyResultController implements Controller {
     constructor(private readonly loadSurveyById: LoadSurveyById) {}
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const { surveyId } = httpRequest.params;
+        try {
+            const { surveyId } = httpRequest.params;
 
-        const survey = await this.loadSurveyById.loadById(surveyId);
-        if (!survey) return forbidden(new InvalidParamError('surveyId'));
+            const survey = await this.loadSurveyById.loadById(surveyId);
+            if (!survey) return forbidden(new InvalidParamError('surveyId'));
 
-        return null;
+            return null;
+        } catch (error) {
+            return serverError(error);
+        }
     }
 }
