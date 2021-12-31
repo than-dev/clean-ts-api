1d9a200cdd0f4c123eb2b054094c40eeae271c99
NathanCotrim <nathan.cotrim@gmail.com>
2021-12-31 16:10:40 -0300

    refactor: create test handle to mock repositories from log

--- src/main/decorators/log-controller-decorator.spec.ts
+++ src/main/decorators/log-controller-decorator.spec.ts
@@ -7,6 +7,7 @@ import {
 import { serverError, ok } from '@/presentation/helpers/http/http-helper';
 import { LogErrorRepository } from '@/data/protocols/db/log/log-error-repository';
 import { mockAccountModel } from '@/domain/test';
+import { mockLogErrorRepository } from '@/data/test';
 
 const makeController = (): Controller => {
     class ControllerStub implements Controller {
@@ -15,15 +16,6 @@ const makeController = (): Controller => {
         }
     }
     return new ControllerStub();
-};
-
-const makeLogErrorRepository = (): LogErrorRepository => {
-    class LogErrorRepositoryStub implements LogErrorRepository {
-        async logError(stack: string): Promise<void> {
-            return new Promise((resolve) => resolve());
-        }
-    }
-    return new LogErrorRepositoryStub();
 };
 
 const makeFakeRequest = (): HttpRequest => ({
@@ -49,7 +41,7 @@ type SutTypes = {
 
 const makeSut = (): SutTypes => {
     const controllerStub = makeController();
-    const logErrorRepositoryStub = makeLogErrorRepository();
+    const logErrorRepositoryStub = mockLogErrorRepository();
     const sut = new LogControllerDecorator(
         controllerStub,
         logErrorRepositoryStub
