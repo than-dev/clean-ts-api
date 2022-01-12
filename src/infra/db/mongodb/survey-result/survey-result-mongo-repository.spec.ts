dc4064f13ae2831712f53b8979b870700fa0547b
NathanCotrim <nathan.cotrim@gmail.com>
2022-01-12 16:17:03 -0300

    refactor: use pattern build to improve mongo query

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.spec.ts
@@ -1,5 +1,5 @@
 import { MongoHelper } from '../helpers/mongo-helper';
-import { Collection } from 'mongodb';
+import { Collection, ObjectId } from 'mongodb';
 import makeFakeDate from 'mockdate';
 import { SurveyResultMongoRepository } from './survey-result-mongo-repository';
 import { SurveyModel } from '@/domain/models/survey';
@@ -84,16 +84,20 @@ describe('Survey Result Mongo Repository', () => {
             });
 
             expect(surveyResult).toBeTruthy();
-            expect(surveyResult.id).toBeTruthy();
-            expect(surveyResult.answer).toBe(survey.answers[0].answer);
+            expect(surveyResult.surveyId).toEqual(survey.id);
+            expect(surveyResult.answers[0].count).toBe(1);
+            expect(surveyResult.answers[0].percent).toBe(100);
+            expect(surveyResult.answers[0].answer).toEqual(
+                survey.answers[0].answer
+            );
         });
 
         it("should update survey result if it's not new", async () => {
             const survey = await makeSurvey();
             const account = await makeAccount();
-            const response = await surveyResultCollection.insertOne({
-                surveyId: survey.id,
-                accountId: account.id,
+            await surveyResultCollection.insertOne({
+                surveyId: new ObjectId(survey.id),
+                accountId: new ObjectId(account.id),
                 answer: survey.answers[0].answer,
                 date: new Date()
             });
@@ -107,8 +111,12 @@ describe('Survey Result Mongo Repository', () => {
             });
 
             expect(surveyResult).toBeTruthy();
-            expect(surveyResult.id).toEqual(response.insertedId);
-            expect(surveyResult.answer).toBe(survey.answers[1].answer);
+            expect(surveyResult.surveyId).toEqual(survey.id);
+            expect(surveyResult.answers[0].answer).toEqual(
+                survey.answers[1].answer
+            );
+            expect(surveyResult.answers[0].count).toBe(1);
+            expect(surveyResult.answers[0].percent).toBe(100);
         });
     });
 });
