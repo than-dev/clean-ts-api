ac10b07988ef2ed010aa886295576a847a4702b7
NathanCotrim <nathan.cotrim@gmail.com>
2022-01-14 07:29:28 -0300

    feat: ensure DbLoadSurveyResult returns a surveyResult on success

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -10,7 +10,8 @@ export class DbLoadSurveyResult implements LoadSurveyResult {
     ) {}
 
     async load(surveyId: string): Promise<SurveyResultModel> {
-        await this.loadSurveyResultRespository.loadBySurveyId(surveyId);
-        return null;
+        const surveyResult =
+            await this.loadSurveyResultRespository.loadBySurveyId(surveyId);
+        return surveyResult;
     }
 }
