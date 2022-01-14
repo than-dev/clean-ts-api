93b59bcc65cab36378c5975e33e47d8636dcecbc
NathanCotrim <nathan.cotrim@gmail.com>
2022-01-14 07:26:44 -0300

    test: ensure DbLoadSurveyResult throws if LoadSurveyResultRepository throws

--- src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
+
+import { DbLoadSurveyResult } from './db-load-survey-result';
 import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result';
 import { mockLoadSurveyResultRepositoryStub } from '@/data/test/mock-db-load-survey-result';
-import { DbLoadSurveyResult } from './db-load-survey-result';
+import { throwError } from '@/domain/test';
 
 type SutTypes = {
     sut: DbLoadSurveyResult;
@@ -28,5 +31,17 @@ describe('DbLoadSurveyResult UseCase', () => {
         await sut.load('any_survey_id');
 
         expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id');
+    });
+
+    it('should throws if LoadSurveyResultRepository throws', async () => {
+        const { sut, loadSurveyResultRepositoryStub } = makeSut();
+        jest.spyOn(
+            loadSurveyResultRepositoryStub,
+            'loadBySurveyId'
+        ).mockImplementationOnce(throwError);
+
+        const promise = sut.load('any_survey_id');
+
+        expect(promise).rejects.toThrow();
     });
 });
