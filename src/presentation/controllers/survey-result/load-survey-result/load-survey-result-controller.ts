c4880095142331d1805804b46a5fa545c96678ff
NathanCotrim <nathan.cotrim@gmail.com>
2022-01-15 15:52:08 -0300

    feat: ensure LoadSurveyResultController returns 500 if LoadSurveyById throws

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -2,17 +2,22 @@ import {
     Controller,
     HttpRequest,
     HttpResponse,
-    LoadSurveyById
+    LoadSurveyById,
+    serverError
 } from './load-survey-result-controller-protocols';
 
 export class LoadSurveyResultController implements Controller {
     constructor(private readonly loadSurveyById: LoadSurveyById) {}
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-        const { surveyId } = httpRequest.body;
+        try {
+            const { surveyId } = httpRequest.body;
 
-        await this.loadSurveyById.loadById(surveyId);
+            await this.loadSurveyById.loadById(surveyId);
 
-        return null;
+            return null;
+        } catch (error) {
+            return serverError(error);
+        }
     }
 }
