3aa81d0cb0a29fe9f1a1341596e9908e242b4f4a
NathanCotrim <nathan.cotrim@gmail.com>
2022-01-15 16:25:45 -0300

    feat: ensure LoadSurveyResultController  call LoadSurveyResult with correct values

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,3 +1,4 @@
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result';
 import {
     Controller,
     forbidden,
@@ -9,17 +10,22 @@ import {
 } from './load-survey-result-controller-protocols';
 
 export class LoadSurveyResultController implements Controller {
-    constructor(private readonly loadSurveyById: LoadSurveyById) {}
+    constructor(
+        private readonly loadSurveyById: LoadSurveyById,
+        private readonly loadSurveyResult: LoadSurveyResult
+    ) {}
 
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
-            const { surveyId } = httpRequest.body;
+            const { surveyId } = httpRequest.params;
 
             const survey = await this.loadSurveyById.loadById(surveyId);
 
             if (!survey) {
                 return forbidden(new InvalidParamError('surveyId'));
             }
+
+            await this.loadSurveyResult.load(surveyId);
 
             return null;
         } catch (error) {
--- src/presentation/test/mock-survey-result.ts
+++ src/presentation/test/mock-survey-result.ts
@@ -1,9 +1,12 @@
 import { SurveyResultModel } from '@/domain/models/survey-result';
-import { mockSurveyResultModel } from '@/domain/test';
+import { mockSurveyModel, mockSurveyResultModel } from '@/domain/test';
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result';
 import {
     SaveSurveyResult,
     SaveSurveyResultParams
 } from '@/domain/usecases/survey-result/save-survey-result';
+import { LoadSurveyById } from '@/domain/usecases/survey/load-survey-by-id';
+import { SurveyModel } from '../controllers/survey/load-survey/load-surveys-controller-protocols';
 
 export const mockSaveSurveyResult = (): SaveSurveyResult => {
     class SaveSurveyResultStub implements SaveSurveyResult {
@@ -13,4 +16,24 @@ export const mockSaveSurveyResult = (): SaveSurveyResult => {
     }
 
     return new SaveSurveyResultStub();
+};
+
+export const mockLoadSurveyResult = (): LoadSurveyResult => {
+    class LoadSurveyResultStub implements LoadSurveyResult {
+        async load(surveyId: string): Promise<SurveyResultModel> {
+            return Promise.resolve(mockSurveyResultModel());
+        }
+    }
+
+    return new LoadSurveyResultStub();
+};
+
+export const mockLoadSurveyById = (): LoadSurveyById => {
+    class LoadSurveyByIdStub implements LoadSurveyById {
+        async loadById(id: string): Promise<SurveyModel> {
+            return Promise.resolve(mockSurveyModel());
+        }
+    }
+
+    return new LoadSurveyByIdStub();
 };
