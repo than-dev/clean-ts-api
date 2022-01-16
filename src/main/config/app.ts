52d43652270c4790749dbc03602e985201ac6b16
NathanCotrim <nathan.cotrim@gmail.com>
2022-01-16 11:08:29 -0300

    feat: setup graphql server

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import { setupAppolloServer } from './apollo-server';
 import setupMiddlewares from './middlewares';
 import setupRoutes from './routes';
 
 const app = express();
+
+setupAppolloServer(app);
 setupMiddlewares(app);
 setupRoutes(app);
+
 export default app;
