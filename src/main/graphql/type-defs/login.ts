e5465a49c8a9c114df73b0b3b5220e6e87642b41
NathanCotrim <nathan.cotrim@gmail.com>
2022-01-16 11:23:02 -0300

    feat: add signUp mutation

--- src/main/graphql/type-defs/login.ts
+++ src/main/graphql/type-defs/login.ts
@@ -1,12 +1,21 @@
-import { gql } from 'apollo-server-express'
+import { gql } from 'apollo-server-express';
 
 export default gql`
-  extend type Query {
-    login (email: String!, password: String!): Account!
-  }
+    extend type Query {
+        login(email: String!, password: String!): Account!
+    }
 
-  type Account {
-    accessToken: String!
-    name: String!
-  }
-`
+    type Account {
+        accessToken: String!
+        name: String!
+    }
+
+    extend type Mutation {
+        signUp(
+            name: String!
+            email: String!
+            password: String!
+            passwordConfirmation: String!
+        ): Account!
+    }
+`;
